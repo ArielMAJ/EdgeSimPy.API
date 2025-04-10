191d2315f1f3a204bec1a569257ab483ccb92ae1
ArielMAJ <ariel.maj@hotmail.com>
2025-04-10 01:21:03 -0300

    BREAKING CHANGE: remove unused packages, endpoints and files to reduce package size

--- api/app.py
+++ api/app.py
@@ -1,13 +1,9 @@
 from contextlib import asynccontextmanager
 from pathlib import Path
 
-from api.config import Config
 from api.entrypoints import router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_async_sqlalchemy import SQLAlchemyMiddleware
-
-# from fastapi.responses import JSONResponse
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
 from loguru import logger
@@ -34,7 +30,6 @@ def get_app() -> FastAPI:
         title="fastapi-backend-template",
         description="FastAPI backend template.",
         lifespan=lifespan,
-        # default_response_class=JSONResponse,
     )
 
     _app.add_middleware(
@@ -43,12 +38,6 @@ def get_app() -> FastAPI:
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
-    )
-    _app.add_middleware(
-        SQLAlchemyMiddleware,
-        db_url=Config.DATABASE.DB_URL,
-        engine_args=Config.DATABASE.ENGINE_ARGS,
-        commit_on_exit=True,
     )
     _app.include_router(router=router)
 
