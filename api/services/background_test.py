a6831a17a0ec16b64301ba64a3ed55a0328910f9
ArielMAJ <ariel.maj@hotmail.com>
2025-04-10 00:44:51 -0300

    fix: adjust test endpoint for background process

--- api/services/background_test.py
+++ api/services/background_test.py
@@ -1,12 +1,19 @@
 import multiprocessing
+import time
 
+from fastapi import BackgroundTasks
 from loguru import logger
 
 
+def test_background_task():
+    logger.info("Starting background task")
+    time.sleep(2)
+    logger.info("Background task completed")
+
+
 class TestService:
-    async def test_background(self):
-        simulation_process = multiprocessing.Process(
-            target=lambda: logger.info("Background process ran")
-        )
+    async def test_background(self, background_task: BackgroundTasks):
+        simulation_process = multiprocessing.Process(target=test_background_task)
         simulation_process.start()
-        simulation_process.join()
+
+        background_task.add_task(simulation_process.join)
